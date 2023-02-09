88a06e9b4356841ef7f4e024b01e505d7a2f148b
Johan Rade <johan.rade@gmail.com>
2023-02-09 18:33:30 +0100

    Added .qrc and .pro files

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("main.qml")));
+    engine.load("qrc:/main.qml");
     if (engine.rootObjects().isEmpty())
         return -1;
 
